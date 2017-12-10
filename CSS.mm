38f29183f7bbea473638abfb2e0e5df0ef4bb551
tom.wang <tom.wang.sz@qq.com>
2017-12-10 22:46:10 +0800

    update

--- CSS.mm
+++ CSS.mm
@@ -1,7 +1,7 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1508632335752" ID="ID_1329756387" MODIFIED="1508632339626" TEXT="CSS">
-<node CREATED="1508632343612" ID="ID_1665614072" MODIFIED="1508632347203" POSITION="right" TEXT="&#x5e03;&#x5c40;">
+<node CREATED="1508632343612" FOLDED="true" ID="ID_1665614072" MODIFIED="1512309334505" POSITION="right" TEXT="&#x5e03;&#x5c40;">
 <node CREATED="1508632410750" ID="ID_1143115227" MODIFIED="1508632436172" TEXT="&#x76d2;&#x6a21;&#x578b;&#xff1a;display&#x3001;position&#x3001;float"/>
 <node CREATED="1508632459794" ID="ID_1632702899" MODIFIED="1508680172361">
 <richcontent TYPE="NODE"><html>
@@ -145,8 +145,7 @@
       &#20801;&#35768;&#21333;&#20010;&#39033;&#30446;&#26377;&#19982;&#20854;&#20182;&#39033;&#30446;&#19981;&#19968;&#26679;&#30340;&#23545;&#40784;&#26041;&#24335;&#65292;&#21487;&#35206;&#30422;align-items&#23646;&#24615;&#12290;&#40664;&#35748;&#20540;&#20026;auto&#65292;&#34920;&#31034;&#32487;&#25215;&#29238;&#20803;&#32032;&#30340;align-items&#23646;&#24615;&#65292;&#22914;&#26524;&#27809;&#26377;&#29238;&#20803;&#32032;&#65292;&#21017;&#31561;&#21516;&#20110;stretch&#12290;
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
 <node CREATED="1508732039652" ID="ID_1180562697" MODIFIED="1508748974668">
 <richcontent TYPE="NODE"><html>
@@ -161,9 +160,121 @@
       1&#12289;
     </p>
   </body>
-</html>
-</richcontent>
+</html></richcontent>
 </node>
+</node>
+<node CREATED="1511522670348" ID="ID_345027687" MODIFIED="1512105427431" POSITION="left" TEXT="MDN">
+<node CREATED="1511522676547" ID="ID_927071010" MODIFIED="1511524761647">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      &#21464;&#24418;transform
+    </p>
+    <p>
+      1&#12289;&#22914;&#26524;&#23558;none&#20043;&#22806;&#30340;&#20540;&#25351;&#23450;&#32473;transform&#65292;&#21017;&#20250;&#21019;&#24314;&#19968;&#20010;stacking context&#65292;&#31867;&#20284;&#20110;position:fixed&#19968;&#26679;
+    </p>
+    <p>
+      2&#12289;transform-origin&#65306;&#29992;&#20110;&#35774;&#32622;&#21407;&#28857;&#65292;&#40664;&#35748;&#22312;&#20803;&#32032;&#30340;&#20013;&#38388;&#65292;&#22914;&#35774;&#32622;&#25104;&#8220;bottom left&#8221;&#21487;&#20197;&#23558;&#21407;&#28857;&#35774;&#32622;&#22312;&#24038;&#19979;&#35282;&#12290;
+    </p>
+    <p>
+      3&#12289;transform&#65306;&#21487;&#20197;&#21516;&#26102;&#35774;&#32622;&#22810;&#20010;&#21464;&#24418;&#65292;&#29992;&#31354;&#26684;&#38548;&#24320;&#65292;&#36825;&#26679;&#21487;&#20197;&#23558;&#20960;&#20010;&#21464;&#24418;&#21516;&#26102;&#24212;&#29992;&#21040;&#20803;&#32032;&#19978;&#12290;
+    </p>
+    <p>
+      4&#12289;perspective&#65306;&#36879;&#35270;&#20540;&#65292;&#20915;&#23450;&#20102;3D&#30340;&#23454;&#29616;&#26041;&#24335;&#65292;&#20803;&#32032;&#21644;&#35266;&#23519;&#32773;&#20043;&#38388;&#30340;&#36317;&#31163;&#36234;&#36828;&#65292;&#20182;&#20204;&#23601;&#20250;&#36234;&#23567;&#12290;
+    </p>
+    <p>
+      5&#12289;perspective-origin&#65306;&#29992;&#20110;&#37197;&#32622;&#35266;&#30475;&#32773;&#30340;&#20301;&#32622;&#65292;&#40664;&#35748;&#34987;&#35748;&#20026;&#26159;&#35266;&#23519;&#32773;&#30340;&#20013;&#24515;&#65292;&#20294;&#36825;&#24448;&#24448;&#38656;&#35201;&#35843;&#25972;&#12290;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1512309377576" ID="ID_1569870557" MODIFIED="1512309384878" POSITION="left" TEXT="&#x56fe;&#x89e3;CSS3&#x9605;&#x8bfb;&#x7b14;&#x8bb0;">
+<node CREATED="1511344075739" ID="ID_837391380" MODIFIED="1511344088918" TEXT="&#x7b2c;&#x4e00;&#x7ae0;&#xff1a;&#x63ed;&#x5f00;CSS3&#x7684;&#x9762;&#x7eb1;"/>
+<node CREATED="1511344093085" FOLDED="true" ID="ID_1830397007" MODIFIED="1512108514194" TEXT="&#x7b2c;&#x4e8c;&#x7ae0;&#xff1a;CSS3&#x9009;&#x62e9;&#x5668;">
+<node CREATED="1511344188911" ID="ID_697578958" MODIFIED="1511366916141" TEXT="&amp;lt;html&amp;gt;&#xa;  &amp;lt;body&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      2.4&amp;#x3001;&amp;#x52a8;&amp;#x6001;&amp;#x4f2a;&amp;#x7c7b;&amp;#x9009;&amp;#x62e9;&amp;#x5668;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      2.4.1&amp;#x3001;&amp;#x52a8;&amp;#x6001;&amp;#x4f2a;&amp;#x7c7b;&amp;#x9009;&amp;#x62e9;&amp;#x5668;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :link &amp;#x8d85;&amp;#x94fe;&amp;#x63a5;&amp;#x6ca1;&amp;#x6709;&amp;#x88ab;&amp;#x8bbf;&amp;#x95ee;&amp;#x8fc7;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :visited &amp;#x8d85;&amp;#x94fe;&amp;#x63a5;&amp;#x5e76;&amp;#x5df2;&amp;#x7ecf;&amp;#x88ab;&amp;#x8bbf;&amp;#x95ee;&amp;#x8fc7;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :active &amp;#x88ab;&amp;#x6fc0;&amp;#x6d3b;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :hover &amp;#x9f20;&amp;#x6807;&amp;#x60ac;&amp;#x505c;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :focus &amp;#x83b7;&amp;#x5f97;&amp;#x7126;&amp;#x70b9;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      2.5&amp;#x3001;&amp;#x76ee;&amp;#x6807;&amp;#x4f2a;&amp;#x7c7b;&amp;#x9009;&amp;#x62e9;&amp;#x5668;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :target &amp;#x5f53;&amp;#x5b58;&amp;#x5728;URL&amp;#x6307;&amp;#x5411;&amp;#x5339;&amp;#x914d;&amp;#x5143;&amp;#x7d20;&amp;#x65f6;&amp;#xff0c;&amp;#x6837;&amp;#x5f0f;&amp;#x6548;&amp;#x679c;&amp;#x624d;&amp;#x4f1a;&amp;#x751f;&amp;#x6548;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      2.6&amp;#x3001;&amp;#x8bed;&amp;#x8a00;&amp;#x4f2a;&amp;#x7c7b;&amp;#x9009;&amp;#x62e9;&amp;#x5668;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :lang(&amp;lt;content ename=&amp;quot;content&amp;quot;&amp;gt;) &amp;#x5339;&amp;#x914d;lang&amp;#x5c5e;&amp;#x6027;&amp;#x8bbe;&amp;#x7f6e;&amp;#x4e3a;zh&amp;#x7684;&amp;#x5143;&amp;#x7d20;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      2.7&amp;#x3001;UI&amp;#x5143;&amp;#x7d20;&amp;#x72b6;&amp;#x6001;&amp;#x4f2a;&amp;#x7c7b;&amp;#x9009;&amp;#x62e9;&amp;#x5668;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :checked &amp;#x5339;&amp;#x914d;&amp;#x9009;&amp;#x4e2d;&amp;#x7684;&amp;#x590d;&amp;#x9009;&amp;#x6846;&amp;#x6216;&amp;#x5355;&amp;#x9009;&amp;#x6309;&amp;#x94ae;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :enabled &amp;#x5339;&amp;#x914d;&amp;#x6240;&amp;#x6709;&amp;#x542f;&amp;#x7528;&amp;#x7684;&amp;#x8868;&amp;#x5355;&amp;#x5143;&amp;#x7d20;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :disabled &amp;#x5339;&amp;#x914d;&amp;#x6240;&amp;#x6709;&amp;#x7981;&amp;#x7528;&amp;#x7684;&amp;#x8868;&amp;#x5355;&amp;#x5143;&amp;#x7d20;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      2.8&amp;#x3001;&amp;#x7ed3;&amp;#x6784;&amp;#x4f2a;&amp;#x7c7b;&amp;#x9009;&amp;#x62e9;&amp;#x5668;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :first-child &amp;#x4f5c;&amp;#x4e3a;&amp;#x7236;&amp;#x5143;&amp;#x7d20;&amp;#x7684;&amp;#x7b2c;&amp;#x4e00;&amp;#x4e2a;&amp;#x5b50;&amp;#x5143;&amp;#x7d20;&amp;#x51fa;&amp;#x73b0;&amp;#xff0c;&amp;#x7b49;&amp;#x540c;&amp;#x4e8e;:nth-child(1)&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :last-child &amp;#x4f5c;&amp;#x4e3a;&amp;#x7236;&amp;#x5143;&amp;#x7d20;&amp;#x7684;&amp;#x6700;&amp;#x540e;&amp;#x4e00;&amp;#x4e2a;&amp;#x5b50;&amp;#x5143;&amp;#x7d20;&amp;#x51fa;&amp;#x73b0;&amp;#xff0c;&amp;#x7b49;&amp;#x540c;&amp;#x4e8e;:nth-last-child(1)&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :root &amp;#x5339;&amp;#x914d;&amp;#x6839;&amp;#x5143;&amp;#x7d20;&amp;#xff0c;HTML&amp;#x6587;&amp;#x6863;&amp;#x4e2d;&amp;#x59cb;&amp;#x7ec8;&amp;#x662f;html&amp;#x5143;&amp;#x7d20;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :nth-child(n) &#xa;      &amp;#x5339;&amp;#x914d;&amp;#x7b2c;n&amp;#x4e2a;&amp;#x5b50;&amp;#x5143;&amp;#x7d20;&amp;#xff0c;n&amp;#x7684;&amp;#x8d77;&amp;#x59cb;&amp;#x503c;&amp;#x662f;1&amp;#x800c;&amp;#x4e0d;&amp;#x662f;0&amp;#xff0c;&amp;#x5982;&amp;#x679c;&amp;#x6307;&amp;#x5b9a;0&amp;#x5219;&amp;#x4f1a;&amp;#x5bfc;&amp;#x81f4;&amp;#x5339;&amp;#x914d;&amp;#x4e0d;&amp;#x5230;&amp;#x4efb;&amp;#x4f55;&amp;#x5143;&amp;#x7d20;&amp;#x3002;n&amp;#x53ef;&amp;#x4ee5;&amp;#x662f;&amp;#x6574;&amp;#x6570;&amp;#x3001;&amp;#x5173;&amp;#x952e;&amp;#x5b57;&amp;#xff08;odd&amp;#x3001;even&amp;#xff09;&amp;#x3001;&amp;#x53ef;&amp;#x4ee5;&amp;#x662f;&amp;#x8868;&amp;#x8fbe;&amp;#x5f0f;&amp;#xff08;2n+1&amp;#x3001;-n+5&amp;#xff0c;&amp;#x8868;&amp;#x8fbe;&amp;#x5f0f;&amp;#x4e2d;&amp;#x7684;n&amp;#x4ece;0&amp;#x5f00;&amp;#x59cb;&amp;#x8ba1;&amp;#x7b97;&amp;#xff09;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :nth-last-child(n) &amp;#x540c;&amp;#x4e0a;&amp;#xff0c;&amp;#x65b9;&amp;#x5411;&amp;#x76f8;&amp;#x53cd;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :nth-of-type(n) &amp;#x5339;&amp;#x914d;&amp;#x6307;&amp;#x5b9a;&amp;#x7c7b;&amp;#x578b;&amp;#xff08;&amp;#x5982;div&amp;#x662f;&amp;#x4e00;&amp;#x4e2a;&amp;#x7c7b;&amp;#x578b;&amp;#xff09;&amp;#x7684;&amp;#x7b2c;n&amp;#x4e2a;&amp;#x5143;&amp;#x7d20;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :nth-last-of-type(n) &amp;#x540c;&amp;#x4e0a;&amp;#xff0c;&amp;#x65b9;&amp;#x5411;&amp;#x76f8;&amp;#x53cd;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :first-of-type&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :last-of-type&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :only-child &amp;#x5339;&amp;#x914d;&amp;#x7236;&amp;#x5143;&amp;#x7d20;&amp;#x4e2d;&amp;#x4ec5;&amp;#x5305;&amp;#x542b;&amp;#x4e00;&amp;#x4e2a;&amp;#x5b50;&amp;#x5143;&amp;#x7d20;&amp;#x7684;&amp;#x5b50;&amp;#x5143;&amp;#x7d20;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :only-of-type&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :empty &amp;#x5339;&amp;#x914d;&amp;#x6ca1;&amp;#x6709;&amp;#x5b50;&amp;#x5143;&amp;#x7d20;&amp;#x4e14;&amp;#x4e0d;&amp;#x5305;&amp;#x542b;&amp;#x6587;&amp;#x672c;&amp;#x8282;&amp;#x70b9;&amp;#x7684;&amp;#x5143;&amp;#x7d20;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      2.9&amp;#x3001;&amp;#x5426;&amp;#x5b9a;&amp;#x4f2a;&amp;#x7c7b;&amp;#x9009;&amp;#x62e9;&amp;#x5668;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      :not(selector) &amp;#x5339;&amp;#x914d;selector&amp;#x4e4b;&amp;#x5916;&amp;#x7684;&amp;#x5143;&amp;#x7d20;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      2.10&amp;#x3001;&amp;#x4f2a;&amp;#x5143;&amp;#x7d20;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      ::first-letter &amp;#x5339;&amp;#x914d;&amp;#x7b2c;&amp;#x4e00;&amp;#x4e2a;&amp;#x5b57;&amp;#x6bcd;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      ::first-line &amp;#x5339;&amp;#x914d;&amp;#x7b2c;&amp;#x4e00;&amp;#x884c;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      ::before &amp;#x5339;&amp;#x914d;&amp;#x5143;&amp;#x7d20;&amp;#x4e4b;&amp;#x524d;&amp;#x7684;&amp;#x4f4d;&amp;#x7f6e;&amp;#xff0c;before&amp;#x548c;after&amp;#x63d2;&amp;#x5165;&amp;#x5185;&amp;#x5bb9;&amp;#x9700;&amp;#x8981;&amp;#x548c;content&amp;#x5c5e;&amp;#x6027;&amp;#x914d;&amp;#x5408;&amp;#x4f7f;&amp;#x7528;&amp;#xff0c;&amp;#x63d2;&amp;#x5165;&amp;#x7684;&amp;#x5185;&amp;#x5bb9;&amp;#x4e0d;&amp;#x4f1a;&amp;#x52a0;&amp;#x5165;DOM&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      ::after &amp;#x5339;&amp;#x914d;&amp;#x5143;&amp;#x7d20;&amp;#x4e4b;&amp;#x540e;&amp;#x7684;&amp;#x4f4d;&amp;#x7f6e;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      ::selection &amp;#x5339;&amp;#x914d;&amp;#x9009;&amp;#x4e2d;&amp;#x7684;&amp;#x6587;&amp;#x672c;&amp;#xff0c;&amp;#x4ec5;&amp;#x63a5;&amp;#x53d7;&amp;#x4e24;&amp;#x4e2a;&amp;#x5c5e;&amp;#x6027;&amp;#xff0c;&amp;#x4e00;&amp;#x4e2a;background&amp;#x548c;&amp;#x4e00;&amp;#x4e2a;color&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      2.11&amp;#x3001;&amp;#x5c5e;&amp;#x6027;&amp;#x9009;&amp;#x62e9;&amp;#x5668;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      [attr] &amp;#x5339;&amp;#x914d;&amp;#x5305;&amp;#x542b;attr&amp;#x5c5e;&amp;#x6027;&amp;#x7684;&amp;#x5143;&amp;#x7d20;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      [attr=val] &amp;#x5339;&amp;#x914d;attr&amp;#x5c5e;&amp;#x6027;&amp;#x503c;&amp;#x4e3a;val&amp;#x7684;&amp;#x5143;&amp;#x7d20;&amp;#xff0c;&amp;#x5fc5;&amp;#x987b;&amp;#x5b8c;&amp;#x5168;&amp;#x76f8;&amp;#x7b49;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      [attr|=val] &amp;#x5339;&amp;#x914d;attr&amp;#x5c5e;&amp;#x6027;&amp;#x503c;&amp;#x7b49;&amp;#x4e8e;val&amp;#x6216;&amp;#x8005;val-&amp;#x5f00;&amp;#x5934;&amp;#x7684;&amp;#x5143;&amp;#x7d20;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      [attr~=val] &amp;#x5339;&amp;#x914d;attr&amp;#x5c5e;&amp;#x6027;&amp;#x503c;&amp;#x4e2d;&amp;#xff08;&amp;#x6709;&amp;#x4e9b;&amp;#x5c5e;&amp;#x6027;&amp;#x6709;&amp;#x591a;&amp;#x4e2a;&amp;#x503c;&amp;#x7528;&amp;#x7a7a;&amp;#x683c;&amp;#x5206;&amp;#x5f00;&amp;#xff09;&amp;#x6709;value&amp;#x7684;&amp;#x5143;&amp;#x7d20;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      [attr*=val] &amp;#x5339;&amp;#x914d;attr&amp;#x5c5e;&amp;#x6027;&amp;#x4efb;&amp;#x610f;&amp;#x4f4d;&amp;#x7f6e;&amp;#x5305;&amp;#x542b;val&amp;#x7684;&amp;#x5143;&amp;#x7d20;&amp;#xff0c;&amp;#x5339;&amp;#x914d;&amp;#x5c40;&amp;#x90e8;&amp;#x5c31;&amp;#x597d;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      [attr^=val] &amp;#x5339;&amp;#x914d;attr&amp;#x5c5e;&amp;#x6027;&amp;#x503c;&amp;#x4ee5;val&amp;#x5f00;&amp;#x5934;&amp;#x7684;&amp;#x5143;&amp;#x7d20;&amp;#xff0c;&amp;#x5339;&amp;#x914d;&amp;#x5c40;&amp;#x90e8;&amp;#x5c31;&amp;#x597d;&#xa;    &amp;lt;/p&amp;gt;&#xa;    &amp;lt;p&amp;gt;&#xa;      [attr$=val] &amp;#x5339;&amp;#x914d;attr&amp;#x5c5e;&amp;#x6027;&amp;#x503c;&amp;#x4ee5;val&amp;#x7ed3;&amp;#x5c3e;&amp;#x7684;&amp;#x5143;&amp;#x7d20;&amp;#xff0c;&amp;#x5339;&amp;#x914d;&amp;#x5c40;&amp;#x90e8;&amp;#x5c31;&amp;#x597d;&#xa;    &amp;lt;/p&amp;gt;&#xa;  &amp;lt;/body&amp;gt;&#xa;&amp;lt;/html&amp;gt;"/>
+</node>
+<node CREATED="1511366942788" FOLDED="true" ID="ID_671777121" MODIFIED="1512309431272" TEXT="&#x7b2c;&#x4e09;&#x7ae0;&#xff1a;&#x8fb9;&#x6846;">
+<node CREATED="1511450879609" ID="ID_212155292" MODIFIED="1511451905240">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      3.1&#12289;&#36793;&#26694;&#31616;&#20171;
+    </p>
+    <p>
+      3.1.1&#12289;&#36793;&#26694;&#30340;&#22522;&#26412;&#23646;&#24615;
+    </p>
+    <p>
+      1&#12289;border-width&#65292;border-color&#65292;border-style
+    </p>
+    <p>
+      2&#12289;&#32553;&#20889;border: border-width border-style border-color&#65292;style&#26159;&#24517;&#39035;&#30340;&#65292;&#22914;&#26524;&#19981;&#20889;&#23427;&#30340;&#40664;&#35748;&#20540;&#26159;none&#65292;&#34920;&#31034;&#27809;&#26377;&#36793;&#26694;&#12290;width&#30340;&#40664;&#35748;&#20540;&#26159;medium&#65288;&#22823;&#32422;3-4px&#65289;&#12290;color&#30340;&#40664;&#35748;&#20540;&#23601;&#26159;&#23383;&#20307;&#30340;&#39068;&#33394;&#12290;
+    </p>
+    <p>
+      3&#12289;&#21333;&#29420;&#35774;&#32622;border-***&#23646;&#24615;&#65292;&#38656;&#36981;&#24490;TRBL&#21407;&#21017;&#65288;TOP/RIGHT/BOTTOM/LEFT&#65289;&#21407;&#21017;&#65292;&#22914;&#65306;
+    </p>
+    <p>
+      border-style: solid dotted dashed inset //4&#20010;&#20540;&#20998;&#21035;&#23545;&#24212;TRBL
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1511452649465" ID="ID_1274553358" MODIFIED="1511453141452">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      3.2&#12289;CSS3&#36793;&#26694;&#39068;&#33394;&#23646;&#24615;&#65306;border-color
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1511452670018" ID="ID_575722976" MODIFIED="1511453130939">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      3.3&#12289;CSS3&#22270;&#29255;&#36793;&#26694;&#23646;&#24615;&#65306;border-image
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1511452901934" ID="ID_865184474" MODIFIED="1511453096057">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      3.4&#12289;CSS3&#22278;&#35282;&#36793;&#26694;&#23646;&#24615;&#65306;border-radius&#23646;&#24615;
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1511452982424" ID="ID_459021139" MODIFIED="1511453059788">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      3.5&#12289;CSS3&#30418;&#23376;&#38452;&#24433;&#23646;&#24615;&#65306;box-shadow
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1512309409885" ID="ID_1654483147" MODIFIED="1512309427391" TEXT="&#x7b2c;&#x5341;&#x56db;&#x7ae0;&#xff1a;&#x5a92;&#x4f53;&#x7279;&#x6027;&#x548c;Responsive&#x8bbe;&#x8ba1;"/>
 </node>
 </node>
 </map>
